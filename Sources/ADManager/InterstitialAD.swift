6ea46d0e476206bb702affe8e9b0e0ac9f7f41b9
Youngho Joo <creative@yonsei.ac.kr>
2024-03-12 20:09:27 +0900

    Add code related to full-page advertisement and reward advertisement

---
+++ Sources/ADManager/InterstitialAD.swift
@@ -0,0 +1,47 @@
+//
+//  InterstitialAD.swift
+//  
+//
+//  Created by JooYoungho on 3/12/24.
+//
+
+import Foundation
+import GoogleMobileAds
+
+public final class InterstitialAD: NSObject, GADFullScreenContentDelegate {
+    var interstitialAD: GADInterstitialAd?
+    var completion: (() -> Void)?
+    var adUnitID: String
+
+    public init(adUnitID: String) {
+        self.adUnitID = adUnitID
+        super.init()
+        loadAD()
+    }
+
+    public func loadAD() {
+        let request = GADRequest()
+        GADInterstitialAd.load(withAdUnitID: self.adUnitID, request: request) { (ad, error) in
+            if let error = error {
+                print("Rewarded ad failed to load with error: \(error.localizedDescription)")
+                return
+            }
+            self.interstitialAD = ad
+            self.interstitialAD?.fullScreenContentDelegate = self
+        }
+    }
+
+    public func showAD(completion: @escaping () -> Void) {
+        self.completion = completion
+        if let ad = interstitialAD {
+            ad.present(fromRootViewController: UIApplication.shared.keyWindowPresentedController!)
+        } else {
+            print("Ad wasn't ready")
+        }
+    }
+
+    public func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
+        completion?()
+        loadAD()
+    }
+}
---
+++ Sources/ADManager/RewardedAD.swift
@@ -0,0 +1,46 @@
+//
+//  RewardedAD.swift
+//
+//
+//  Created by JooYoungho on 3/12/24.
+//
+
+import Foundation
+import GoogleMobileAds
+
+public final class RewardedAD: NSObject, GADFullScreenContentDelegate {
+    var rewardedAD: GADRewardedAd?
+    var completion: (() -> Void)?
+    var adUnitID: String
+
+    public init(adUnitID: String) {
+        self.adUnitID = adUnitID
+        super.init()
+        loadAD()
+    }
+
+    public func loadAD() {
+        let request = GADRequest()
+        GADRewardedAd.load(withAdUnitID: self.adUnitID, request: request) { [weak self] (ad, error) in
+            guard let self = self else { return }
+            if let error = error {
+                print("Rewarded ad failed to load with error: \(error.localizedDescription)")
+                return
+            }
+            self.rewardedAD = ad
+            self.rewardedAD?.fullScreenContentDelegate = self
+        }
+    }
+
+    public func showAD(completion: @escaping () -> Void) {
+        self.completion = completion
+        if let ad = rewardedAD {
+            ad.present(fromRootViewController: UIApplication.shared.keyWindowPresentedController!) { [weak self] in
+                self?.completion?()
+                self?.loadAD()
+            }
+        } else {
+            print("Ad wasn't ready")
+        }
+    }
+}
